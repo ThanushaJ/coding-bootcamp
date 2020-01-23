ced5778a0d591a161df20ffda523f563e07d00ff
ThanushaJ <thanusnani@gmail.com>
2020-01-23 20:58:41 +0530

    NaturalNumbersWhile_TC

--- LoopingProblems/NaturalNumbersWhile.py
+++ LoopingProblems/NaturalNumbersWhile.py
@@ -1,3 +1,12 @@
+"""Write a program to print all natural numbers from 1 to n. - using while loop
+Positive TC:
+Input: 6
+Output: 1 2 3 4 5 6
+Negative TC:
+Input: 6
+Output: 1 2 3 4 5
+"""
+
 startNum = 1
 endNum = int(input())
 while startNum <= endNum:
