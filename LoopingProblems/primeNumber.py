d3deac6154d2a27a45d37e0c16112c83037c535b
ThanushaJ <thanusnani@gmail.com>
2020-01-24 09:02:44 +0530

    Prime Number

--- LoopingProblems/primeNumber.py
+++ LoopingProblems/primeNumber.py
@@ -1,2 +1,13 @@
 """Write a program to check whether a number is Prime number or not.
 """
+numP = int(input())
+
+if numP>1:
+    for count in range(2,numP):
+        if numP%count==0:
+            print("Not Prime")
+            break
+    else:
+        print("prime")
+else:
+    print("Not Prime")
