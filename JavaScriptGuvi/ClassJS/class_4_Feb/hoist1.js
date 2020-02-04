8cda7e6e60c1eced9d60ea4eb704dba28fcd2c93
ThanushaJ <thanusnani@gmail.com>
2020-02-04 13:39:08 +0530

    Home Task 4th Feb

---
+++ JavaScriptGuvi/ClassJS/class_4_Feb/hoist1.js
@@ -0,0 +1,6 @@
+function hoist() {
+  console.log(a, b);
+  a = 10;
+  var b = 20;
+  console.log(a, b);
+};
---
+++ JavaScriptGuvi/ClassJS/class_4_Feb/prog2.js
@@ -0,0 +1,6 @@
+function strict() {
+  console.log(b);
+  'use strict';
+  var b = 20;
+  console.log(b);
+};
